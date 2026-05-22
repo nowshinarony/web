6bf895539cab4c42d2069f3c63b6045d2029f206
Nowshin Nawar Arony <arony.nowshin@gmail.com>
2026-05-22 15:15:25 -0700

    add Icon component with SVG support

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import content from '@/data/content-en.json'
-import Icon from '@/components/Icon.vue'
 
 const router = useRouter()
 </script>
@@ -16,6 +15,6 @@ const router = useRouter()
       <h1>{{ content.title }}</h1>
     </button>
     <p class="pl-2">{{ content.tagline }}</p>
-    <Icon name="eye" />
+    
   </div>
 </template>
